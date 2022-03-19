a9f90c33e018a71d49e8a84254385fdf03221fd3
aniket-sinha8 <sinhaaniket32@gmail.com>
2022-03-19 22:18:36 +0530

    Quiz App Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Questions } from './Questions';
+import Button from '@mui/material/Button';
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+
+    if(nextQuestion < Questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  }
+
+  const restart = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+      <div className='quiz-arena'>
+        {showScore ? (
+          <div className='score-arena'>
+            <h2>Your score is {score} out of {Questions.length}!!</h2>
+            <Button color='success' variant="contained" onClick={restart}>Restart</Button>
+          </div>
+        ) : (
+          <div>
+            <div className='question-section'>
+              <div className='question-count'>
+                <span>Question {currentQuestion + 1}</span>/{Questions.length}
+              </div>
+              <div className='question-arena'>{Questions[currentQuestion].question}</div>
+            </div>
+            
+            <div className='answer-arena'>
+              {Questions[currentQuestion].options.map((option) => (
+                <span className='button' onClick={() => handleClick(option.isCorrect)}>
+                  <Button color='success' variant="outlined">{option.answer}</Button>
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Questions.js
@@ -0,0 +1,47 @@
+export const Questions = [
+    {
+        question: 'Who is the founder of GeeksforGeeks?',
+        options: [
+            { answer: 'Satya Nadella', isCorrect: false },
+            { answer: 'Ankush Singla', isCorrect: false },
+            { answer: 'Sandeep Jain', isCorrect: true },
+            { answer: 'Ashneer Grover', isCorrect: false },
+        ]
+    },
+    {
+        question: 'Who is CEO of Apple?',
+        options: [
+            { answer: 'Jeff Bezos', isCorrect: false },
+            { answer: 'Elon Musk', isCorrect: false },
+            { answer: 'Bill Gates', isCorrect: false },
+            { answer: 'Tim Cook', isCorrect: true },
+        ]
+    },
+    {
+        question: 'Who was the first Prime Minister of India?',
+        options: [
+            { answer: 'Jawaharlal Nehru', isCorrect: true },
+            { answer: 'Narendra Modi', isCorrect: false },
+            { answer: 'Rajendra Prasad', isCorrect: false },
+            { answer: 'Indira Gandhi', isCorrect: false },
+        ]
+    },
+    {
+        question: 'What is 2+2?',
+        options: [
+            { answer: '1', isCorrect: false },
+            { answer: '4', isCorrect: true },
+            { answer: '6', isCorrect: false },
+            { answer: '7', isCorrect: false },
+        ]
+    },
+    {
+        question: 'What is the full name of Go language?',
+        options: [
+            { answer: 'Go', isCorrect: false },
+            { answer: 'Golang', isCorrect: true },
+            { answer: 'GoLanguage', isCorrect: false },
+            { answer: 'GoScript', isCorrect: false },
+        ]
+    }
+];
